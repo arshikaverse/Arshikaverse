1f370898ceef5cbec0d3ebf3dd2701e3621c01d2
Atharv Singh <atharvsin03@gmail.com>
2026-06-08 10:24:24 +0530

    Arshikaverse Website Model 1.0

---
+++ js/footer.js
@@ -0,0 +1,15 @@
+document.write(`
+  <footer>
+    <div class="container text-center">
+      <p class="mb-2">Find me online and let's talk about books!</p>
+      <div class="d-flex justify-content-center gap-4">
+        <a href="https://www.youtube.com/@arshikaverse" target="_blank" class="footer-link fs-4"><i
+            class="fab fa-youtube"></i></a>
+        <a href="https://www.goodreads.com/user/show/154143838-arshika" target="_blank" class="footer-link fs-4"><i
+            class="fab fa-goodreads-g"></i></a>
+      </div>
+      <p>Developed by <a href="https://atharvsi03.github.io/atharvswebservices/" id="credit-link"
+          class="text-decoration-none text-white">Atharv's Website Services</a></p>
+    </div>
+  </footer>
+`)
